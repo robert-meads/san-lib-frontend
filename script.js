7426ea1e65a305fa4eb84f3468f6649f37051705
Stina Aygol <StinaAygol@proton.me>
2023-05-15 16:06:44 -0700

    Added some more input fields.

--- script.js
+++ script.js
@@ -1,87 +1,61 @@
-// Goal: Takes user to a website.
-// Pre: User knows website domain name.
-// Post: User is at target website.
-// Function visits(domainName) {
-// 	Go to domainName.
-// }
-
 // Add submit eventlistener.
 const searchForm = document.getElementById('search_form');
-const firstNameEl = document.getElementById('first_name');
-let firstName;
+const nameEl = document.getElementById('name');
+const titleEl = document.getElementById('title');
+const yearEl = document.getElementById('year');
+const genreEl = document.getElementById('genre');
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log('hello from submit button inside search_form!');
-  firstName = firstNameEl.value;
-  console.log(firstName);
 
-  let obj = enterSearchQuery(firstName, '', '');
+  let obj = enterSearchQuery(nameEl.val, titleEl.val, yearEl.val, genreEl.val);
   console.log(obj);
 });
-
-// Goal: Take user to library search bar which is a form of attributes to narrow search results.
-// Pre:
-// Post: User is at library search bar.
-// I think this is just a link that takes you to the search page. No JS needed.
-// function searchForBook() {
-// Take user to library search bar.
-// UI pseudocode:
-/*
-    User is taken to a page containing a form of fields.
-    User enters info.
-    User clicks 'search' button.
-    Form collects all info and packages them into an object.
-    Return object.
-    */
-// }
-
-// Goal: Bundle up search data into one object.
-// Pre: At least one of the search parameters, besides year, is included.
-// Post: Return an object with data bundled together.
-function enterSearchQuery(author, title, year) {
-  // Create object.
-  // Add author, title, year to object.
-  // Return object.
-  return {
-    author,
-    title,
-    year,
-  };
-}
 
 const catalog = [
   {
     author: 'JK Rowling',
     title: "Harry Potter and the Sorcerer's Stone",
     year: 1999,
+    genre: 'sci-fi',
     count: 2,
   },
   {
     author: 'Rick Riordan',
     title: 'The Lightning Thief (Percy Jackson and the Olympians, #1)',
     year: 2011,
+    genre: 'teens',
     count: 1,
   },
   {
     author: 'Lemony Snicket',
     title: 'The Bad Beginning (A Series of Unfortunate Events, #1)',
     year: 2001,
+    genre: 'kids',
     count: 4,
   },
   {
     author: 'Christopher Paolini',
     title: 'Eragon (The Inheritance Cycle, #1)',
+    genre: 'teens',
     year: 2004,
     count: 0,
   },
   {
     author: 'Eoin Colfer',
     title: 'Artemis Fowl (Artemis Fowl, #1)',
+    genre: 'kids',
     year: 2003,
     count: 3,
   },
-  { author: 'Cornelia Funke', title: 'Inkheart', year: 2002, count: 1 },
+  {
+    author: 'Cornelia Funke',
+    title: 'Inkheart',
+    genre: 'kids',
+    year: 2002,
+    count: 1,
+  },
 ];
 
 const sampleFormData = {
@@ -90,6 +64,21 @@ const sampleFormData = {
   sampleFormData2 = {
     year: 2001,
   };
+
+// Goal: Bundle up search data into one object.
+// Pre: At least one of the search parameters, besides year, is included.
+// Post: Return an object with data bundled together.
+function enterSearchQuery(author, title, year, genre) {
+  // Create object.
+  // Add author, title, year to object.
+  // Return object.
+  return {
+    author,
+    title,
+    year,
+    genre,
+  };
+}
 
 // Goal: Uses search query object to look through catalog for relevant books.
 // Pre: search query object exists.
@@ -150,17 +139,3 @@ function updateCatalog(patronReceipt) {
   // Add return date to patronReceipt.
   // Return patronReceipt
 }
-// // Should this be two separate functions? One to make changes to lib catalog and another to aggregate data for receipt? Idk…
-
-function start() {
-  for (let i = 0; i < 10; i++) {
-    setTimeout(function () {
-      console.log('AA');
-    }, 2000);
-  }
-  console.log('Inside Start()');
-}
-
-start();
-console.log('hello world');
-console.log('bye world');
