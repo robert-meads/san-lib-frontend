e98ea7b4ad8cacb93989609a64909198e39af1de
Stina Aygol <StinaAygol@proton.me>
2023-06-01 10:53:00 -0700

    Template strings don't work well with console.log(). Stick to plain console.log(string, obj) form.

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ bookResultsEl.addEventListener('submit', (e) => {
   // get all books that are to be checked out
   const checkedBooks = getCheckedBooks();
 
+  // Template strings don't work well with console.log().
+  // console.log(`Here are the checked books: ${checkedBooks}`);
+  // console.log(`Here are the checked books: ${JSON.stringify(checkedBooks)}`);
 });
 
 function getCheckedBooks() {
