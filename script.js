9488ee4c57ae4df477478072f14e636a0280f24a
Stina Aygol <StinaAygol@proton.me>
2023-06-04 04:29:15 -0700

    Finished updateCatalog feature.

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ bookResultsForm.addEventListener('submit', (e) => {
 
   // update catalog
   updateCatalog(checkedBooks);
+
+  // Print receipt
+  console.log('Check the count values: ', catalog);
 });
 
 function getCheckedBooks() {
@@ -188,15 +191,25 @@ function displayBooks(listOfBooks) {
 // Post: List of books that user wants  to borrow is noted in library catalog system.
 function updateCatalog(booksBorrowed) {
   booksBorrowed.forEach((borrowed_book) => {
-    let checkedBook = borrowed_book.children[1].children[3].innerText;
+    // checkedBook is book.id.
+    let id = borrowed_book.children[1].children[3].innerText.split(' ');
+    console.log('id ', id);
+    // The format for id is always "id: <number>". So we can always assume the second value of id array is the id.
+    let checkedBook = id[1];
+    console.log('checkedBook: ', checkedBook);
     let found = false;
 
     for (let book_in_catalog of catalog) {
+      console.log('Book in catalog: ', book_in_catalog);
       if (checkedBook == book_in_catalog.id) {
         if (book_in_catalog.count !== 0) {
+          console.log('Decreasing by 1.');
           book_in_catalog.count -= 1;
         } else if (book_in_catalog.count === 0) {
-          console.log('Do you want to put book on hold?');
+          console.log('catalog book count is 0.');
+          console.log(
+            `Do you want to put ${getBookProperties(borrowed_book)} on hold?`
+          );
         }
         found = true;
         break;
@@ -204,9 +217,12 @@ function updateCatalog(booksBorrowed) {
     }
     if (!found) {
       console.log(
-        'Book does not exist in catalog. Do you want the library to order the book?'
+        `${getBookProperties(
+          borrowed_book
+        )} does not exist in catalog. Do you want the library to order the book?`
       );
     }
+    console.log('\n');
   });
 }
 
